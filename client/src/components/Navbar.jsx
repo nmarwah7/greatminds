209ba912144e65471fadffb60c322297211378e6
roshnidaksha <groshnidaksha2@yahoo.com>
2026-01-16 23:28:53 +0800

    Edit navbar and add more details to participant description for staffs

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import './Navbar.css';
+import './../index.css';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext.jsx';
 
 export default function Navbar() {
-  return (
-    <nav className="navbar">
-      <div className="navbar-title">GreatMiNDs</div>
-      <div className="navbar-links">
-        <span>Programs</span>
-        <span>Calendar</span>
-        <span>About</span>
-        <button className="home-btn">Home</button>
-      </div>
-    </nav>
-  );
+    const navigate = useNavigate();
+    const { user, loading } = useAuth();
+
+    return (
+        <nav className="navbar">
+            <div className="navbar-title">GreatMiNDs</div>
+            {!loading && (
+                <div className="navbar-links">
+                    {user ? (
+                        <>
+                            <button className="programs-btn">Programs</button>
+                            <button className="programs-btn" onClick={() => navigate('/dashboard')}>
+                                Calendar
+                            </button>
+                            <button className="programs-btn">About</button>
+                            <button className="home-btn">Home</button>
+                        </>
+                    ) : (
+                        <button className="home-btn" onClick={() => navigate('/login')}>Login</button>
+                    )}
+                </div>
+            )}
+        </nav>
+    );
 }
